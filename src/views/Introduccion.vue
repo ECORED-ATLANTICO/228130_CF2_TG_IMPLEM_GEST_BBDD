5868feffd9d24e5c46cba43a09f73803951b17c4
Carlos Andres Diaz Pinto <98177573+AndresDiazPinto@users.noreply.github.com>
2026-02-26 08:01:26 -0500

    Update Introduccion.vue
    Se deja nuevo link del video con ajustes

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -24,10 +24,10 @@
 
     figure.mb-5
       .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/h3yn79WUEb4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/ek2SgbCn1yw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
       figcaption Video: Introducción a la seguridad de bases de datos
 
-
+      
 </template>
 
 <script>
